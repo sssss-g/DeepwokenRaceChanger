2a35bf19219b0f55ae153164d0b8cfd0c617ad58
sssss-g <125706985+sssss-g@users.noreply.github.com>
2023-02-17 18:25:31 -0500

    Add files via upload
    race assets uploaded

---
+++ Faces.lua
@@ -0,0 +1,85 @@
+return {
+    ["Canor"] =    {
+       ["Anxious"] = "rbxassetid://7111901791",
+       ["Cross"] = "rbxassetid://6328854364",
+       ["Determined"] = "rbxassetid://6328853552",
+       ["Irritated"] = "rbxassetid://7111902881"
+    },
+    ["Capra"] =    {
+       ["Calm"] = "rbxassetid://7369677392",
+       ["Serious"] = "rbxassetid://7382798790"
+    },
+    ["Chrysid"] =    {
+       ["Cheerful"] = "http://www.roblox.com/asset/?id=10981233796",
+       ["Nonchalant"] = "rbxassetid://10980351537"
+    },
+    ["Etrean"] =    {
+       ["Carefree"] = "rbxassetid://6453803599",
+       ["Concerned"] = "http://www.roblox.com/asset/?id=4635021914",
+       ["Confident"] = "http://www.roblox.com/asset/?id=5613622669",
+       ["Furious"] = "rbxassetid://6453721904",
+       ["NarrowIrritated"] = "rbxassetid://7966385334",
+       ["Serious"] = "http://www.roblox.com/asset/?id=4635031569",
+       ["Unimpressed"] = "http://www.roblox.com/asset/?id=4635022187"
+    },
+    ["Ganymede"] =    {
+       ["Cheerful"] = "rbxassetid://10953981105",
+       ["Crossed"] = "rbxassetid://10953953527",
+       ["Crosses"] = "rbxassetid://6815221383",
+       ["Curious"] = "http://www.roblox.com/asset/?id=5022060426",
+       ["Diamonds"] = "rbxassetid://10953891712",
+       ["Dizzy"] = "rbxassetid://10968287198",
+       ["Focused"] = "rbxassetid://6815231424",
+       ["Lashes"] = "rbxassetid://6815190971",
+       ["Ring"] = "rbxassetid://8221962943",
+       ["Sparkles"] = "rbxassetid://10953937083",
+       ["Tired"] = "rbxassetid://6815164037"
+    },
+    ["Vesperian"] =    {
+      ["Cheerful"] = "rbxassetid://10953981105",
+      ["Crossed"] = "rbxassetid://10953953527",
+      ["Crosses"] = "rbxassetid://6815221383",
+      ["Curious"] = "http://www.roblox.com/asset/?id=5022060426",
+      ["Diamonds"] = "rbxassetid://10953891712",
+      ["Dizzy"] = "rbxassetid://10968287198",
+      ["Focused"] = "rbxassetid://6815231424",
+      ["Lashes"] = "rbxassetid://6815190971",
+      ["Ring"] = "rbxassetid://8221962943",
+      ["Sparkles"] = "rbxassetid://10953937083",
+      ["Tired"] = "rbxassetid://6815164037"
+   },
+    ["Heliodar"] =    {
+       ["Concerned"] = "rbxassetid://11175778533",
+       ["Confident"] = "rbxassetid://11160028351",
+       ["Furious"] = "rbxassetid://11175782044",
+       ["Scarred"] = "rbxassetid://11175786468",
+       ["Serious"] = "rbxassetid://11175793040",
+       ["Unimpressed"] = "rbxassetid://11175796694"
+    },
+    ["Lightborn"] =    {
+       ["Carefree"] = "rbxassetid://6466179729",
+       ["Concerned"] = "rbxassetid://7121650945",
+       ["Confident"] = "rbxassetid://6466188578",
+       ["Furious"] = "rbxassetid://6466177150",
+       ["Scarred"] = "rbxassetid://7121651491",
+       ["Serious"] = "rbxassetid://6466191371",
+       ["Unimpressed"] = "rbxassetid://7121652576"
+    },
+    ["Other"] =    {
+       ["Carefree"] = "rbxassetid://6453740192",
+       ["Concerned"] = "http://www.roblox.com/asset/?id=4608029036",
+       ["Confident"] = "http://www.roblox.com/asset/?id=5613302434",
+       ["Furious"] = "rbxassetid://6453708608",
+       ["NarrowDisgusted"] = "rbxassetid://7112721305",
+       ["NarrowIrritated"] = "rbxassetid://7112699685",
+       ["Scarred"] = "rbxassetid://6453846341",
+       ["Serious"] = "http://www.roblox.com/asset/?id=4608015412",
+       ["Unimpressed"] = "http://www.roblox.com/asset/?id=4608016592"
+    },
+    ["Tiran"] =    {
+       ["Carefree"] = "rbxassetid://10712358348",
+       ["Confident"] = "rbxassetid://10712340396",
+       ["Furious"] = "rbxassetid://10706568972",
+       ["Disgusted"] = "rbxassetid://10712337908"
+    }
+ }
